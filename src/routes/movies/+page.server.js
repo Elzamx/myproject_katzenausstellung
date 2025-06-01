01d73e190d31ad0b91c07aa4a4c2f048014f655e
Elzamx <miftaelz@students.zhaw.ch>
2025-06-01 22:14:53 +0200

    comment load function

--- src/routes/movies/+page.server.js
+++ src/routes/movies/+page.server.js
@@ -1,6 +1,6 @@
 import db from "$lib/db.js";
 
-export async function load() {
+export async function load() { // stellt einer Svelte-Komponente Daten zur Verfügung
   return {
     katzen: await db.getMovies()
   };

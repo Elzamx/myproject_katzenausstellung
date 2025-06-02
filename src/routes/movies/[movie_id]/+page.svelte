fdb2f9f72b6fe4cd4e58c67b181085720fc0a67b
Elzamx <miftaelz@students.zhaw.ch>
2025-06-02 18:24:56 +0200

    button angepasst

--- src/routes/movies/[movie_id]/+page.svelte
+++ src/routes/movies/[movie_id]/+page.svelte
@@ -6,7 +6,8 @@
 <h1 class="text-center">{katze.name}</h1>
 
 <div class="row mt-3">
-  <div class="col-12 text-center mb-3" > <!-- Katzenbild in der Mitte zentriert -->
+  <div class="col-12 text-center mb-3">
+    <!-- Katzenbild in der Mitte zentriert -->
     <img class="img-fluid cat-image" src={katze.poster} alt="" />
   </div>
 
@@ -20,18 +21,20 @@
       <button class="btn btn-danger">Katze entfernen</button>
     </form>
   </div>
-</div> 
- 
-<div class="backbt-container">
-  <!-- damit ich button zentrieren kann-->
+</div>
+
+<div class="d-flex justify-content-end gap-2 my-3">
+  <!-- Bootstrap Klasse für buttons nebeneinander & rechts -->
+  <a href={"/movies/" + katze._id + "/edit"} class="btn btn-primary"
+    >Katzendaten bearbeiten</a
+  >
   <a href="/movies" class="btn btn-secondary">Zurück zur Übersicht</a>
 </div>
 
 <style>
-.cat-image {
-  width: 350px;
-  height: 350px;
-  border-radius: 8px; /* Abgerundete Ecken */
-}
+  .cat-image {
+    width: 350px;
+    height: 350px;
+    border-radius: 8px; /* Abgerundete Ecken */
+  }
 </style>
-

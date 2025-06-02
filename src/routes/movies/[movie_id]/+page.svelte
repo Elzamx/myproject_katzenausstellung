e15caa0ed1d7a8fa1382c2a350086805209751d3
Elzamx <miftaelz@students.zhaw.ch>
2025-06-02 15:41:15 +0200

    movie_id angepasst

--- src/routes/movies/[movie_id]/+page.svelte
+++ src/routes/movies/[movie_id]/+page.svelte
@@ -6,31 +6,32 @@
 <h1 class="text-center">{katze.name}</h1>
 
 <div class="row mt-3">
-  <div class="col-3">
-    <img class="img-fluid" src={katze.poster} alt="" />
+  <div class="col-12 text-center mb-3" > <!-- Katzenbild in der Mitte zentriert -->
+    <img class="img-fluid cat-image" src={katze.poster} alt="" />
   </div>
 
-  <div class="col">
-    <p>Rasse: {katze.rasse}</p>
-    <p>Alter: {katze.alter}</p>
-    <p>Geschlecht: {katze.geschlecht}</p>
+  <div class="col-12 text-center mb-3">
+    <p><b>Rasse:</b> {katze.rasse}</p>
+    <p><b>Alter:</b> {katze.alter}</p>
+    <p><b>Geschlecht:</b> {katze.geschlecht}</p>
 
     <form method="POST" action="?/delete">
-      <input type="hidden" name="id" value={katze._id}>
+      <input type="hidden" name="id" value={katze._id} />
       <button class="btn btn-danger">Katze entfernen</button>
-    </form>  
+    </form>
   </div>
-</div>
-<div class="backbt-container"> <!-- damit ich button zentrieren kann-->
+</div> 
+ 
+<div class="backbt-container">
+  <!-- damit ich button zentrieren kann-->
   <a href="/movies" class="btn btn-secondary">Zurück zur Übersicht</a>
 </div>
 
 <style>
-.backbt-container {
-  display: flex;
-  justify-content: left;
-  margin-top: 1em;
-  margin-bottom: 1em;
+.cat-image {
+  width: 350px;
+  height: 350px;
+  border-radius: 8px; /* Abgerundete Ecken */
 }
 </style>
 
